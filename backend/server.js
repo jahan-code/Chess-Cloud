1092129cf60ab4f80c9c2c57383a980b7b282cbe
jahan-code <jahanzaib9599@gmail.com>
2025-01-20 05:53:04 +0500

    ready for deployement

--- backend/server.js
+++ backend/server.js
@@ -153,4 +153,4 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(5000, () => console.log(`Server running on http://localhost:${PORT}`));
+server.listen(5000, () => console.log(`Server running on https://cloud-chess.zeabur.app`);
